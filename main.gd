49a854f8457894c2d15279531930b8ee26dc70e0
HiddenKendo <136893770+HiddenKendo@users.noreply.github.com>
2024-04-21 00:32:51 +0700

    move and wrap around screen

--- main.gd
+++ main.gd
@@ -13,7 +13,8 @@ func _ready():
 func spawn_player():
 	var player_instance = player_scene.instantiate()
 	add_child(player_instance)
-	player_instance.global_position = Vector2(randf() * get_viewport_rect().size.x, randf()  * get_viewport_rect().size.y)
+	player_instance.global_position = Vector2(randf() * get_viewport_rect().size.x,
+											  randf()  * get_viewport_rect().size.y)
 	
 	var random_team_index: int = randi_range(0, 2)
 	player_instance.update_texture(random_team_index)
--- player.gd
+++ player.gd
@@ -1,14 +1,28 @@
-extends Node2D
+extends CharacterBody2D
 
 
 enum teams {ROCK, PAPER, SCISSORS}
 
+const SPEED = 100
 var my_team: int
 @onready var sprite_2d = $Sprite2D
 
 @onready var rock_image = preload("res://rock.png")
 @onready var paper_image = preload("res://paper.png")
 @onready var scissors_image = preload("res://scissors.png")
+
+var my_dir: Vector2
+
+
+func _ready():
+	my_dir = Vector2.RIGHT.rotated(randf_range(0, TAU))
+	velocity = my_dir * SPEED
+
+
+func _process(delta):
+	move_and_slide()
+	global_position = Vector2(wrapf(global_position.x, 0, get_viewport_rect().size.x,),
+							  wrapf(global_position.y, 0, get_viewport_rect().size.y,))
 
 
 func update_texture(team_index: int):
